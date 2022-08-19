d13f4b5228980206a14da60d9920f83c4c859d30
Srinivasa Durai <97778015+ScarfaceX73@users.noreply.github.com>
2022-08-19 16:47:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const countdown = document.createElement("h1");
+
+countdown.innerText= "Countdown Starts";
+document.body.append(countdown);
+
+
+
+setTimeout(function() {
+    countdown.innerHTML = 10;
+    setTimeout(function() {
+        countdown.innerHTML = 9;
+        setTimeout(function() {
+           countdown.innerHTML = 8;
+            setTimeout(function() {
+               countdown.innerHTML = 7;
+                setTimeout(function() {
+                    countdown.innerHTML = 6;
+                    setTimeout(function() {
+                       countdown.innerHTML = 5;
+                        setTimeout(function() {
+                            countdown.innerHTML = 4;
+                            setTimeout(function() {
+                               countdown.innerHTML = 3;
+                                setTimeout(function() {
+                                    countdown.innerHTML = 2;
+                                    setTimeout(function() {
+                                        countdown.innerHTML = 1;
+                                        setTimeout(function() {
+                                            countdown.innerHTML = "Happy Independence Day";
+                                        },2000);
+                                    },2000);
+                                },2000);
+                            },2000);
+                        },2000);
+                    },2000);
+                },2000);
+            },2000);
+        },2000);
+    },2000);
+},2000);
